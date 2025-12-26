31c5f46573bd301f22e074da5cdff82a25405930
Qaaxaap <lkn15289353760@126.com>
2025-12-26 16:06:22 +0800

    完成 week4

--- cpp/Daimayuan/week4/CF2118D2.cpp
+++ cpp/Daimayuan/week4/CF2118D2.cpp
@@ -11,16 +11,52 @@ namespace Qaaxaap
 		{
 			int n,k;
 			cin>>n>>k;
-			vector<int> p(n),d(n);
-			for(auto &tmp:p) cin>>tmp;
-			for(auto &tmp:d) cin>>tmp;
-			map<int,vector<int>> zb,fb;
-			for(int i=0;i<n;i++)
+			vector<int> p(n+1),d(n+1),vis(n*2+1);
+			for(int i=1;i<=n;i++) cin>>p[i];
+			for(int i=1;i<=n;i++) cin>>d[i];
+			map<int,int> zb,fb,qu;
+			vector<pair<int,int>> nxt(n+1);
+			vector<vector<int>> mp(n*2+1);
+			for(int i=1;i<=n;i++)
 			{
-				zb[(p[i]-d[i])%k].push_back(i);
-				fb[(p[i]+d[i])%k].push_back(i);
+				nxt[i].first=zb[(p[i]+d[i])%k];
+				if(nxt[i].first) mp[nxt[i].first+n].push_back(i);
+				else mp[0].push_back(i);
+				zb[(p[i]+d[i])%k]=i;
 			}
-			
+			for(int i=n;i>=1;i--) 
+			{	
+				nxt[i].second=fb[(p[i]-d[i]+k)%k];
+				mp[nxt[i].second].push_back(i+n);
+				fb[(p[i]-d[i]+k)%k]=i;
+			}
+			queue<int> q;
+			q.push(0);
+			while(!q.empty())
+			{
+				int u=q.front();
+				q.pop();
+				if(vis[u]) continue;
+				vis[u]=1;
+				for(auto v:mp[u]) q.push(v);
+			}	
+			int qa;
+			cin>>qa;
+			vector<pair<int,int>> a(qa);
+			vector<int> ans(qa);
+			int cnt=0;
+			for(auto &tmp:a) cin>>tmp.first,tmp.second=cnt++;cnt=n;
+			sort(a.rbegin(),a.rend());
+			for(auto [qus,id]:a)
+			{
+				while(cnt>0&&p[cnt]>=qus) qu[(p[cnt]-d[cnt]+k)%k]=cnt,cnt--;
+				ans[id]=vis[qu[qus%k]];
+			}
+			for(auto tmp:ans)
+			{
+				if(tmp) cout<<"Yes"<<endl;
+				else cout<<"No"<<endl;
+			}
 		}
 	}
 }
