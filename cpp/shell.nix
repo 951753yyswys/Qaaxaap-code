fe20a6e3c68bd237707378e9729fa29260ab0006
Qaaxaap <lkn15289353760@126.com>
2026-02-09 20:56:07 +0800

    upd:更换 nix-shell 为 zsh

--- cpp/shell.nix
+++ cpp/shell.nix
@@ -23,6 +23,11 @@ pkgs.mkShell {
     # 调试信息优化
     export CFLAGS="-O2 -g3"
     export CXXFLAGS="-O2 -g3 -std=c++20"
+    if [ -z "$IN_NIX_SHELL" ] || [ -z "$NIX_SHELL_PRESERVE_PROMPT" ]; then
+      export SHELL=${pkgs.zsh}/bin/zsh
+      exec ${pkgs.zsh}/bin/zsh
+    fi
     echo "C++开发环境已激活"
   '';
+    
 }
